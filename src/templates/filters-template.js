0b25ab754498caaae5ffff2f977b2cdc30d4047b
putn1k <skachok11@yandex.ru>
2023-08-28 13:45:36 +0300

    шаблонизировал вывод фильтров

--- src/templates/filters-template.js
+++ src/templates/filters-template.js
@@ -1,25 +1,21 @@
+import {
+  FILTER_TYPES
+} from '../const.js';
+
+import {
+  toCapitalize
+} from '../utils.js';
+
+const createFilterTypeListTemplate = (filterTypeList) => filterTypeList.reduce((markup, type, id)=>`${markup}
+  <div class="trip-filters__filter">
+    <input id="filter-${type}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${type}" ${(id === 0) ? 'checked' : ''}>
+    <label class="trip-filters__filter-label" for="filter-${type}">${toCapitalize(type)}</label>
+  </div>`, '');
+
 function createFiltersTemplate() {
   return (
     `<form class="trip-filters" action="#" method="get">
-      <div class="trip-filters__filter">
-        <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-        <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-        <label class="trip-filters__filter-label" for="filter-future">Future</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present">
-        <label class="trip-filters__filter-label" for="filter-present">Present</label>
-      </div>
-
-      <div class="trip-filters__filter">
-        <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-        <label class="trip-filters__filter-label" for="filter-past">Past</label>
-      </div>
+      ${createFilterTypeListTemplate(FILTER_TYPES)}
 
       <button class="visually-hidden" type="submit">Accept filter</button>
     </form>`
