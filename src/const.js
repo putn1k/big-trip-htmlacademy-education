1fd8541dda74bd0722a5263ceefd3fe9e67d81e4
putn1k <skachok11@yandex.ru>
2023-08-28 13:45:04 +0300

    добавлены типы фильтров

--- src/const.js
+++ src/const.js
@@ -1,6 +1,7 @@
 const CITIES = ['Paris', 'London', 'Chicago', 'Tokio', 'New York', 'Moscow', 'Amsterdam', 'San-Francisco'];
 const DESCRIPTION = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt architecto labore atque!';
 const EVENT_TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
+const FILTER_TYPES = ['everything', 'future', 'present', 'past'];
 const Counts = {
   DESTINATIONS: 10,
   DESCRIPTION_PHOTO: 4,
@@ -11,7 +12,6 @@ const Price = {
   MIN: 1,
   MAX: 500
 };
-
 const Duration = {
   HOUR: 5,
   DAY: 3,
@@ -22,6 +22,7 @@ export {
   CITIES,
   DESCRIPTION,
   EVENT_TYPES,
+  FILTER_TYPES,
   Counts,
   Price,
   Duration
