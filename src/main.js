e15b7b6b52858c7738ae3e57e80084f0ca3b8a44
putn1k <skachok11@yandex.ru>
2023-08-30 15:16:17 +0300

    создал main презентер и инициализирую все презентеры в нем

--- src/main.js
+++ src/main.js
@@ -1,29 +1,5 @@
-import PointsPresenter from './presenter/points-presenter.js';
-import FiltersPresenter from './presenter/filters-presenter.js';
-import TripInfoPresenter from './presenter/trip-info-presenter.js';
-import MockService from './service/mock-service.js';
-import DestinationsModel from './model/destinations-model.js';
-import OffersModel from './model/offers-model.js';
-import PointsModel from './model/points-model.js';
+import MainPresenter from './presenter/main-presenter.js';
 
-const pointsContainer = document.querySelector('.trip-events');
+const mainPresenter = new MainPresenter();
 
-const mockService = new MockService();
-
-const destinationsModel = new DestinationsModel(mockService);
-const offersModel = new OffersModel(mockService);
-const pointsModel = new PointsModel(mockService);
-
-const pointsPresenter = new PointsPresenter({
-  pointsContainer,
-  destinationsModel,
-  offersModel,
-  pointsModel
-});
-
-const filtersPresenter = new FiltersPresenter();
-const tripInfoPresenter = new TripInfoPresenter();
-
-pointsPresenter.init();
-filtersPresenter.init();
-tripInfoPresenter.init();
+mainPresenter.init();
---
+++ src/presenter/main-presenter.js
@@ -0,0 +1,31 @@
+import TripInfoPresenter from './trip-info-presenter.js';
+import FiltersPresenter from './filters-presenter.js';
+import PointsPresenter from './points-presenter.js';
+
+import MockService from '../service/mock-service.js';
+
+import DestinationsModel from '../model/destinations-model.js';
+import OffersModel from '../model/offers-model.js';
+import PointsModel from '../model/points-model.js';
+
+const mockService = new MockService();
+
+const destinationsModel = new DestinationsModel(mockService);
+const offersModel = new OffersModel(mockService);
+const pointsModel = new PointsModel(mockService);
+
+const filtersPresenter = new FiltersPresenter();
+const tripInfoPresenter = new TripInfoPresenter();
+const pointsPresenter = new PointsPresenter({
+  destinationsModel,
+  offersModel,
+  pointsModel,
+});
+
+export default class MainPresenter {
+  init() {
+    filtersPresenter.init();
+    tripInfoPresenter.init();
+    pointsPresenter.init();
+  }
+}
