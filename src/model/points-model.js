2209b684db2e69357bbb80622c736c427149e7c6
putn1k <skachok11@yandex.ru>
2023-09-17 17:51:47 +0300

    добавил методы для модели точек

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,4 +1,5 @@
 import Observable from '../framework/observable.js';
+import {updateItem} from '../utils.js';
 
 export default class PointsModel extends Observable {
   #points = null;
@@ -12,5 +13,27 @@ export default class PointsModel extends Observable {
 
   get() {
     return this.#points;
+  }
+
+  getById(id) {
+    return this.#points.find((point)=>point.id === id);
+  }
+
+  update(updateType, point) {
+    const updatedPoint = this.#service.updatePoint(point);
+    this.#points = updateItem(this.#points, updatedPoint);
+    this._notify(updateType, updatedPoint);
+  }
+
+  add(updateType, point) {
+    const addedPoint = this.#service.addPoint(point);
+    this.#points.push(addedPoint);
+    this._notify(updateType, addedPoint);
+  }
+
+  delete(updateType, point) {
+    this.#service.deletePoint(point);
+    this.#points = this.#points.filter((item)=>item.id !== point.id);
+    this._notify(updateType);
   }
 }
